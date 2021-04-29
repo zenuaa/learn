08c0f2fd69e6d4138213d9e104bde57219d148ca
zenua <zenuaa@outlook.com>
2021-04-30 00:18:33 +0300

    Практика ч.1. Начинаем создавать приложение

--- mainPr/js/1.js
+++ mainPr/js/1.js
@@ -1,1 +1,20 @@
 'use strict';
+ const numberOfFilms = prompt('How many films have you watched?');
+ const personalMovieDB = {
+     count: numberOfFilms,
+     movies: {},
+     actors: {},
+     genres: [],
+     privat: false,
+ };
+
+const questOnePart1 = prompt('One of the last watched films');
+const questOnePart2 = prompt('How mutch do you rate it');
+const questTwoPart1 = prompt('One of the last watched films');
+const questTwoPart2 = prompt('How mutch do you rate it');
+ 
+const movies = {};
+movies[questOnePart1] = questOnePart2;
+movies[questTwoPart1] = questTwoPart2;
+console.log(personalMovieDB);
+console.log(movies);
