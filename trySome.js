17fd1726645a53b37872ee7d92b68522707ef9b1
zenua <zenuaa@outlook.com>
2021-05-03 22:25:22 +0300

    Разбираю замыкания в trySome.js

--- trySome.js
+++ trySome.js
@@ -1,17 +1,60 @@
 " use strict ";
-let x = 'Hi Trehe';
-console.log(x);
-console.log('hi');
-
-let obj = {
-    empty: undefined,
-    '':'empty string',
-    1:10,
-    'astring': 'str',
-
+let x = 0;
+let someFun = function () {
+    let y = 4;
+    return x + y;
 };
 
-console.log('I am - ' + obj.empty);
-console.log(obj);
-console.log(obj['']);
-console.log(obj.variable);
+
+console.log(x);
+console.dir(someFun);
+// function Count(x) {
+//     let f = function (y) {
+//         return ++x;
+//     };
+//     return f;
+// }
+// let countOne = new Count(2);
+// console.log(countOne(3));
+// console.log(countOne(2));
+// console.dir(countOne);
+// let countTwo = new Count(5);
+// console.log(countTwo(5));
+// console.dir(countTwo);
+
+// function mainAdd(valC) {
+      
+//      return function add(someNumber) {
+//         let count;
+//         count = valC + someNumber;
+//         return count;
+//     };
+// }
+// let mainCount = new mainAdd(2);
+// console.log(mainCount(1));
+
+// function Сounter() {
+//     let x = 0,
+//     e = 999;
+//     console.warn(x + ' value of var-x');
+//     let y = function () {
+//         x += e;
+//         console.warn( x+ ' value of var-x++');
+//         return x;
+//     };
+//     return y;
+// }
+// let countOne = new Сounter();
+// console.log('\t'+ countOne() + ' - console');
+// console.dir(countOne);
+
+// console.log('\t'+ countOne() + ' - console');
+// console.log('\t'+ countOne() + ' - console');
+// console.dir(countOne);
+
+// let w = 111;
+// function someFunc() {
+//    let e = 1;
+//     return (w + e);
+// }
+// console.dir(someFunc);
