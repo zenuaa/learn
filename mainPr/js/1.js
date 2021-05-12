f6e214485cd2fd1cb3e98c8af7203ef87c4c7219
zenua <zenuaa@outlook.com>
2021-05-12 22:20:56 +0300

    day 4. Практика 4. Забиваем все действия с обьектом ему в методы..

--- mainPr/js/1.js
+++ mainPr/js/1.js
@@ -1,56 +1,80 @@
 'use strict';
-const numberOfFilms = prompt('How many films have you watched?');
+
 const personalMovieDB = {
-    count: numberOfFilms,
+    count: 0,
     movies: {},
     actors: {},
     genres: [],
     privat: false,
+    start: function () { // ask - How many movies did you watch?
+        this.count = prompt('How many movies have you watched?');
+    },
+    rememberMyFilms: function () { // asking about 2 last watched movies and rate them..
+        let i = 0,
+            questOnePart1,
+            questOnePart2,
+            mass = 'One';
+
+
+        do {
+            questOnePart1 = prompt(mass + ' of the last watched films?').toLocaleLowerCase();
+            if (!questOnePart1 || questOnePart1.length > 50) {
+                continue;
+            }
+            questOnePart2 = prompt('How mutch do you rate it?');
+            if (!questOnePart2 || isNaN(questOnePart2)) {
+                continue;
+            }
+            this.movies[questOnePart1] = questOnePart2;
+            mass = 'Second';
+            i++;
+        }
+        while (i < 2);
+    },
+    detectPersonalLevel: function () { //detect watchmen lvl
+        if (personalMovieDB.count < 10) {
+            console.log('Too few movies watched.');
+        } else if (personalMovieDB.count > 9 && personalMovieDB.count < 31) {
+            console.log('You are a classic viewer.');
+        } else if (personalMovieDB.count > 30) {
+            console.log('You are a movie fan.');
+        } else {
+            console.log('An arror has occurrated.');
+        }
+    },
+    showMyDB: function () { // return this object
+        if (!this.privat) {
+            return this;
+        }
+    },
+    toggleVisibleMyDB: function () { // chage this.privat to true or false
+        if (this.privat === false) {
+            this.privat = true;
+        } else {
+            this.privat = false;
+        }
+    },
+    writeYourGenres: function () { // asking about your 3 favorit genres and show them to console..
+        for (let i = 0, q; i < 3; i++) {
+            q = prompt(`Your favorit genre by number ${i+1}`).toLocaleLowerCase();
+            if (!q) {
+                i--;
+                continue;
+            }
+            this.genres[i] = q;
+        }
+        // let i = 0;
+        // while (i < 3) {
+        //     let q = prompt(`Your favorit genre by number ${i+1}`);
+        //     if (!q) {
+        //         continue;
+        //     }
+        //     this.genres[i] = q;
+        //     i++;
+        // }
+        this.genres.forEach(function (item, index) {
+            console.log(`Favorit genres #${index + 1} - it's ${item}.`);
+        });
+    },
+
 };
-let i = 0,
-    questOnePart1,
-    questOnePart2,
-    mass = 'One';
-const movies = {};
-
-do {
-    questOnePart1 = prompt(mass + ' of the last watched films?');
-    if (!questOnePart1 || questOnePart1.length > 5) {
-        continue;
-    }
-    questOnePart2 = prompt('How mutch do you rate it?');
-    if (!questOnePart2) {
-        continue;
-    }
-    movies[questOnePart1] = questOnePart2;
-    mass = 'Second';
-    i++;
-}
-while (i < 2);
-
-if (personalMovieDB.count < 10) {
-    console.log('Too few movies watched.');
-} else if (personalMovieDB.count > 9 && personalMovieDB.count < 31) {
-    console.log('You are a classic viewer.');
-} else if (personalMovieDB.count > 30) {
-    console.log('You are a movie fan.');
-} else {
-    console.log('An arror has occurrated.');
-}
-
-function showMyDB() {
-    if (!personalMovieDB.privat) {
-        console.log(personalMovieDB);
-    }
-}
-showMyDB();
-
-
-
-function writeYourGenres() {
-    for (let i = 0; i < 3; i++) {
-        personalMovieDB.genres[i] = prompt(`Your favorit genre by number ${i+1}`);
-    }
-}
-writeYourGenres();
-console.log(personalMovieDB);
