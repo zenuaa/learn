362f89019c301938bc171279b8ef762609f9d46d
zenua <zenuaa@outlook.com>
2022-01-28 01:13:45 +0200

    lesson 52 AJAX - XMLHttpRequest

--- js/jsConvert.js
+++ js/jsConvert.js
@@ -1,2 +1,20 @@
 'use strict'
 
+const inputUSD = document.querySelector('#convertUSD'),
+        inputUAH = document.querySelector('#convertUAH');
+inputUSD.addEventListener('input', ()=>{
+    const request = new XMLHttpRequest();
+    request.open('GET', 'js/current.json');
+    request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+    request.send();
+    request.addEventListener('load', ()=>{
+        if(request.status === 200){
+            // console.log(request.response);  
+            const data = JSON.parse(request.response);
+            // console.log(data.current.usd);
+            inputUAH.value = (inputUSD.value * data.current.usd).toFixed(2);
+        }
+        else{inputUAH.value = 'что-то пошло не так'}
+    })
+
+})
